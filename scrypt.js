86008e8b8ead732eec3f063e20bab4db1b515c0d
Ortanov <77912808+Ortanov@users.noreply.github.com>
2025-01-17 22:07:59 +0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+const icon = document.querySelector(".icon");
+const search = document.querySelector(".search");
+const clear =  document.querySelector('.clear'); 
+icon.onclick = function () {
+  search.classList.toggle("active");
+  clear.classList.toggle("open");
+};
+
